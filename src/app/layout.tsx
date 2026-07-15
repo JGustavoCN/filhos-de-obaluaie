8739603e3196ceb003227b6ee018267c0fec58ed
JGustavoCN <josegustavocorreianascimento@gmail.com>
2026-07-15 17:06:44 -0300

    fix: mount VisualEditing unconditionally to fix iframe connection issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -90,7 +90,7 @@ export default async function RootLayout({
       <body>
         {children}
         <SanityLive />
-        {isDraftMode && <VisualEditing />}
+        <VisualEditing />
       </body>
     </html>
   );
